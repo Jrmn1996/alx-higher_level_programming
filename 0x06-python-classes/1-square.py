bb28dd6b0dcdfe9bc864e74147a5c911675d5d68
Jrmn1996 <jrmn1996@gmail.com>
2023-09-26 23:39:27 +0000

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#/usr/bin/python3
+"""square module"""
+class square:
+    """defines square"""
+    def __init__(self, size):
+        """constructor
+        arguments:
+               size: length of a square's side
+        """
+        self.__size = size
