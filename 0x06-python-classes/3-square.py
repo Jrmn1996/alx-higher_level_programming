fc3894b9a00e8359f4720912cbd151ea2c9ba05e
Jrmn1996 <jrmn1996@gmail.com>
2023-09-27 00:34:33 +0000

    Area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""square module"""
+
+class Square:
+    """defines a square"""
+    
+    def __init__(self, size=0):
+        """constructor
+
+        arguments:
+            size: length of square's side
+
+        raises:
+            TypeError: if size is not an integer
+            ValueError: if size < 0
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
