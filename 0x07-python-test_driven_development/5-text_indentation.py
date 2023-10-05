9642be4de898be961d50e7671545280d5d5b3283
Jrmn1996 <jrmn1996@gmail.com>
2023-10-05 11:35:44 +0000

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -17,6 +17,7 @@ def text_indentation(text):
             [line.strip(" ") for line in text.split(delim)])
     print(text, end="")
 
+
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/5-text_indentation.txt")
