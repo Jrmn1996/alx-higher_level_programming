c2c543059aed21f1d6deb8092833d8d827cb037d
Jrmn1996 <jrmn1996@gmail.com>
2023-10-10 16:59:48 +0000

    Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module for is_kind_of_class method"""
+
+
+def is_kind_of_class(obj, a_class):
+    """determines if an obj is a subclass of a class"""
+    return isinstance(obj, a_class)
