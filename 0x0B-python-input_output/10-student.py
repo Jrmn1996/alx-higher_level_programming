7902c2949bcf32f7485173e49bd88ba078069a09
Jrmn1996 <jrmn1996@gmail.com>
2023-10-10 22:52:35 +0000

    Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -23,7 +23,7 @@ class Student:
         except Exception:
             return self.__dict__
         my_dict = dict()
-        for key, calue in self.__dict__.itrms():
+        for key, value in self.__dict__.items():
             if key in attrs:
                 my_dict[key] = value
         return my_dict
