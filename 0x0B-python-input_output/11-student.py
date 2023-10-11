009d1589c2d57f61b5021a62cb2cc3a889670edb
Jrmn1996 <jrmn1996@gmail.com>
2023-10-11 00:44:18 +0000

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""contains class student"""
+
+
+class Student:
+    """rep of a student"""
+    def __init__(self, first_name, last_name, age):
+        """initializes student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dict rep of a student instance
+        if attrs is a list of strs, only attr names contained in this
+        list must be retrieved
+        otherwise all attr must be retrieved
+        """
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        my_dict = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                my_dict[key] = value
+        return my_dict
+
+    def reload_from_json(self, json):
+        """this func replaces all attrs of the student instance with
+        the ones in the json argument"""
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
