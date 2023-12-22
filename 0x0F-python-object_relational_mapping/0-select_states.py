973d90649a6cd4b319de3a63595184fd025665d3
Jrmn1996 <jrmn1996@gmail.com>
2023-12-22 19:54:27 +0000

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur= db.cursor()
     cur.execute("SELECT * FROM states")
-    states = cursor.fetchall()
+    states = cur.fetchall()
     for row in states:
         print(row)
     cur.close()
