5bc031dab6ab967684e85cd2d7ba00921504db22
Jrmn1996 <jrmn1996@gmail.com>
2023-12-23 00:11:39 +0000

    First state model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script takes in the name of a state as an arg and lists all cities
+of that state, using the database hbtn_0e_4_usa"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer,
+                autoincrement=True,
+                primary_key=True)
+    name = Column(String(128),
+                  nullable=False)
