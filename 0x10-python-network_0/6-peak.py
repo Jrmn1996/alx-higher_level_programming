a05423c58dd2737d536ca72fed21d4c67f6d205d
Jrmn1996 <jrmn1996@gmail.com>
2024-01-07 18:26:33 +0000

    6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""defines algorithm yhat finds the peak in a list of unsorted ints"""
+
+
+def find_peak(list_of_integers):
+    """args:
+    list_of_integers: list of unsorted ints
+    """
+    if list_of_integers == []:
+        return None
+
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
